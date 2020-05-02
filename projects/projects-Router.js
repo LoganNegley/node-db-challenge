c36a39b4c842e8287f9c817383d3050aae1ce5be
Logan Negley <logannegley@gmail.com>
2020-05-01 20:24:03 -0500

    endpoint not hitting

--- projects/projects-Router.js
+++ projects/projects-Router.js
@@ -4,13 +4,27 @@ const router = express.Router();
 const db = require('./projects-model');
 
 router.get('/', (req,res) =>{
+    console.log('hello from get')
     db.getProjects()
     .then(projects =>{
         res.status(200).json(projects)
     })
     .catch(error =>{
+        
         res.status(500).json({
             errorMessage: 'Unable to get projects'
+        })
+    })
+})
+
+router.get('/resources', (req,res) =>{
+    db.getResources()
+    .then(resources =>{
+        res.status(200).json(resources)
+    })
+    .catch(error =>{
+        res.status(500).json({
+            errorMessage: 'Unable to get list of resources'
         })
     })
 })
